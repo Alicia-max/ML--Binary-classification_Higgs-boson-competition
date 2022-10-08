72f045d7e7fed82f3834c37deb6ea08ab16d5fe1
Alicia-max <56864437+Alicia-max@users.noreply.github.com>
2022-10-08 17:16:09 +0200

    batch_iter

--- project/helper.py
+++ project/helper.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""some helper functions for project 1."""
+
 import csv
 import numpy as np
 
@@ -18,3 +18,25 @@ def load_csv_data(data_path, sub_sample=False):
         ID = ids[::50]
 
     return y, features, ID
+
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    """
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
