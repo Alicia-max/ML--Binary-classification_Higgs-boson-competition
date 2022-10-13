ececcf0a78fe564e4075e70f2773b25948cd2a01
alexdesko <63108506+alexdesko@users.noreply.github.com>
2022-10-13 15:57:44 +0200

    Finetuning preprocessing + refs/ideas

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
 # ML
 Project 1-ML
 ## TODO
-- Data exploration, preprocessing and exploration
-- Implement all the models
+- Some refs say to discard all the columns that have '_phi_' in it. Should test that.
+- Finetune the preprocessing. You'll see in the histogram after preprocessing that a lot of values are centers in 0. Why the fuck ? should we just remove this ?
+- Maybe try a polynomial augmentation ? (Or fourier augmentation,  works really well with images/sound...)
+- Remove the outliers (I have no clue how to do this)
+- Try 'Mean' and 'Median' for the filling od the missing values
+- Actually implement some models and to some testing
 - ..
 ---
-### Ideas for data preprocessing:
-- First simply normalize the features column-wise  
-- In https://www.kaggle.com/code/sugataghosh/implementing-logistic-regression-from-scratch  
-the author removes the features that are composed of 30 % or more of nans. Then for the other ones, the nans are filled wit either 'Mean or 'Median'. We can try both and compare them, it could be a nice analysis for the report.
-- In https://github.com/dufourc1/ML_CS433_project1/blob/master/src/run.py  
-  they do polynomial feature extraction, no clue why but might be a good Idea ?
-- Assuming we use these two feature processing methods, we could do an ablation study afterwards, which is generally nicely seen in ML papers
+
+### Done by Alex 13.10
+Mainly preprocessing:
+- I've read the article on the physics behind, and from it i removed the features of columns (22 to 28) because they actually do not make any sense! It's actually the columns where there are ~70% of missing values so here ya go
+- The 'jet_num' columns is actually discrete valued feature that categorizes the type of 'event' (physics stuff) that occured. So I don't know If we should discard it or if we should use this info... I'll look more into this!
+- Refactored the code a bit, thx Alicia for your seaborn mastery damn these plots are awesome :O
+
+---
+## Refs
+- https://github.com/phunterlau/kaggle_higgs/tree/97c5b3d6ffebb772bbb6a586f00b485a2dc9355a (25th of original challenge)
+- https://www.kaggle.com/competitions/higgs-boson/discussion/10425 (The winner of the original challenge)
+- https://www.kaggle.com/code/sugataghosh/implementing-logistic-regression-from-scratch  (random dude)
+- https://github.com/dufourc1/ML_CS433_project1/blob/master/src/run.py  (former EPFL student)
--- project/preprocessing.py
+++ project/preprocessing.py
@@ -1,43 +1,78 @@
 import numpy as np
 
-def standarize (x) :
-    
+'''
+Black listed columns ID - Name
+22 - NOT SURE YET (Nbs of jet)
+
+23 - PRI_jet_leading_pt
+24 - PRI_jet_leading_eta
+25 - PRI_jet_leading_phi
+26 - PRI_jet_subleading_pt
+27 - PRI_jet_subleading_eta
+28 - PRI_jet_subleading_phi
+
+29 - NOT SURE YET TOO (Total pt)
+'''
+
+
+def preprocess_data(x):
+    '''
+    Preprocessing the data.
+    Should be applied to the train and test sets separately
+    '''
+    black_listed_columns  = [22, 23, 24, 25, 26, 27, 28]
+    # Should we include 29 ?
+    ## First removing the black-listed columns the columns
+    x = _remove_columns(x, black_listed_columns)
+    ## Filling the missing values with the median of each sets
+    x = _fill_missing_values(x)
+    ## Should we do the outliers here ?
+
+    ## Normalizing the data
+    x_normalized = _standardize(x)
+    return x_normalized
+
+
+
+def _standardize(x):
     centered_data = x - np.mean(x, axis=0)
     std_data = centered_data / np.std(centered_data, axis=0)
-    
     return std_data
 
+def _remove_columns(x, columns):
+    total_nb_columns = x.shape[1]
+    kept_columns =  np.delete(np.arange(total_nb_columns), columns)
+    return x[:, kept_columns]
 
-
-def missing_values (x_train, x_test, th=0.9) : 
-    
+def _fill_missing_values(x, threshold = .8):
+    discarded_cols = []
     #Check missing value according the features 
-    remove_col=[]
-    
-    for j in range(x_train.shape[1]) : 
+    for j in range(x.shape[1]) : 
         
-        feat=x_train[:,j]
-        
+        feat = x[:,j]
         #check % of missing value per col
         miss_perc = len(feat[feat==-999.0])/len(feat)
-        
-        #replace missing value by the median of non missing value
-        if (miss_perc<th): 
-            med=np.median(feat[feat!=-999.0])
-            x_train[:,j] = np.where(feat==-999.0, med, x_train[:,j])
-            x_test[:,j] = np.where(x_test[:,j]==-999.0, med , x_test[:,j])
-        else : 
-            remov_col.append(col)
-                
-    x_train = np.delete(x_train, remove_col, axis = 0)
-    x_test = np.delete(x_test, remove_col, axis = 0)
-  
-        
-    return x_train, x_test
+        if miss_perc > threshold:
+            discarded_cols.append(j)
+        else:
+            median = np.median(feat[feat!=-999.0])
+            x[:,j] = np.where(feat == -999.0, median, x[:,j])
+    
+    x = _remove_columns(x, discarded_cols)
+    return x
         
 def outlier(x):
-    
+    pass
         
         
 def remove_features (x, features) : 
-    
+    pass
+
+
+if __name__ == "__main__":
+    # For testing purposes
+    x = np.random.rand(100,3)
+    print(x)
+    print(_standardize(x))
+    print(_remove_columns(x, [1]))
+    ## OKAY IT WORKS LEZGOOOOOOOOOO
